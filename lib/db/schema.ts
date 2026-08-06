e287360466a524216b143e1cf9ddf458723ad0b3
kishanghosh090 <kishanghosh090@gmail.com>
2026-08-06 22:51:10 +0530

    relations

---
+++ lib/db/schema.ts
@@ -0,0 +1,32 @@
+import {
+  boolean,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { Relation } from "drizzle-orm";
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  path: text("path").notNull(),
+  size: integer("size").notNull(),
+  type: text("type").notNull(),
+
+  fileUrl: text("fileUrl").notNull(),
+  thumbnailUrl: text("thumbnail_url"),
+
+  // ownership
+  userId: text("user_id").notNull(),
+  parentId: uuid("parent_id"), // parent folder id (null for root item)
+
+  isFolder: boolean("is_folder").default(false).notNull(),
+  isstarred: boolean("is_starred").default(false).notNull(),
+  isTrash: boolean("is_trash").default(false).notNull(),
+
+  // timestamps
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
