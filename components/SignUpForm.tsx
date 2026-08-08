abf52b0d089fada4d7adc275a04b6d8a1de44bf5
kishanghosh090 <kishanghosh090@gmail.com>
2026-08-08 23:00:26 +0530

    add signup

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,6 +1,8 @@
 "use client";
-
+import { Card } from "@heroui/react";
 import { useForm } from "react-hook-form";
+
+import { Button, Form, InputOTP, Label, Spinner } from "@heroui/react";
 
 import { useSignUp } from "@clerk/nextjs";
 import { z } from "zod";
@@ -19,6 +21,7 @@ export default function SignUpForm() {
   const [verificationError, seterificationError] = useState<string | null>(
     null,
   );
+  const [isComplete, setIsComplete] = React.useState(false);
 
   const { signUp, isLoaded, setActive } = useSignUp();
 
@@ -34,6 +37,10 @@ export default function SignUpForm() {
       passwordConformation: "",
     },
   });
+  const handleComplete = (code: string) => {
+    setIsComplete(true);
+    console.log("Code complete:", code);
+  };
 
   const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
     if (!isLoaded) {
@@ -98,7 +105,51 @@ export default function SignUpForm() {
   };
 
   if (verifying) {
-    return <h1>this is OTP entering field</h1>;
+    return (
+      <Form
+        className="flex w-[280px] flex-col gap-2"
+        onSubmit={handleVerificationSubmit}
+      >
+        <Label>Verify account</Label>
+        <InputOTP
+          maxLength={6}
+          value={verificationCode}
+          onComplete={handleComplete}
+          onChange={(val) => {
+            setVerificationCode(val);
+            setIsComplete(false);
+          }}
+        >
+          <InputOTP.Group>
+            <InputOTP.Slot index={0} />
+            <InputOTP.Slot index={1} />
+            <InputOTP.Slot index={2} />
+          </InputOTP.Group>
+          <InputOTP.Separator />
+          <InputOTP.Group>
+            <InputOTP.Slot index={3} />
+            <InputOTP.Slot index={4} />
+            <InputOTP.Slot index={5} />
+          </InputOTP.Group>
+        </InputOTP>
+        <Button
+          className="mt-2 w-full"
+          isDisabled={!isComplete}
+          isPending={isSubmitting}
+          type="submit"
+          variant="primary"
+        >
+          {isSubmitting ? (
+            <>
+              <Spinner color="current" size="sm" />
+              Verifying...
+            </>
+          ) : (
+            "Verify Code"
+          )}
+        </Button>
+      </Form>
+    );
   }
   return <h1>Signup form with email and other fields</h1>;
 }
