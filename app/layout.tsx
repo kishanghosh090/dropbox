b31b9c71705f5033bc5f49c250346ce461cffeef
kishanghosh090 <kishanghosh090@gmail.com>
2026-08-10 22:33:42 +0530

    add venthen space

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Dropbox",
+  title: "venthen space",
   description: "Secure cloud storage for your images, powered by ImageKit",
 };
 
